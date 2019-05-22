8305c569c4d80d29ca9cf93a7f924f95f141d3d9
Clement FOLTRAN <clfoltra@e2r8p3.42.fr>
2019-05-22 15:51:10 +0200

    Landing page OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
