66038df0d631284f654828983d9dede3fb4e9de2
Martin <mrtscg@gmail.com>
2019-06-04 10:03:01 +0200

    [PUSH] password modification front + backend finished

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { EnterViewSettingsReturn } from './services/enter-view-settings-return';
 import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
@@ -19,6 +19,7 @@ import { UpdateNotificationsService } from './services/update-notifications/upda
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
+
 export class SettingsComponent implements OnInit {
 
   public resolvedData: EnterViewSettingsReturn;
@@ -30,7 +31,83 @@ export class SettingsComponent implements OnInit {
   public UpdateNameAPIParameter: UpdateNameParameter;
   public UpdateEmailAPIParameter: UpdateEmailParameter;
   public UpdatePasswordAPIParameter: UpdatePasswordParameter;
-  checked = true;
+
+  CheckPassword(event){
+    const password_len_message = document.getElementById("password_len_message");
+    const password_up_message = document.getElementById("password_up_message");
+    const password_num_message = document.getElementById("password_num_message");
+    const password_spe_message = document.getElementById("password_spe_message");
+    var password = (document.getElementById("newPassword") as HTMLInputElement).value; 
+   
+    var anUpperCase = /[A-Z]/;
+    var aLowerCase = /[a-z]/; 
+    var aNumber = /[0-9]/;
+    var aSpecial = /[!|@|#|$|%|^|&|*|(|)|=|+|-|_]/;
+ 
+        if (password.length < 12)
+        {
+            password_len_message.innerHTML = "✗ 12 characters or more";
+            password_len_message.style.color = "red";
+        } else {
+            password_len_message.innerHTML = "✓ 12 characters or more";
+            password_len_message.style.color = "green";
+        }
+
+        var numUpper = 0;
+        var numNums = 0;
+        var numSpecials = 0;
+        for (var i = 0; i < password.length; i++){
+            if (anUpperCase.test(password[i]))
+                numUpper++;
+            else if (aNumber.test(password[i]))
+                numNums++;
+            else if (aSpecial.test(password[i]))
+                numSpecials++;
+        }
+        if (numUpper < 1)
+        {
+            password_up_message.innerHTML = "✗ Upper characters";
+            password_up_message.style.color = "red";
+        } else 
+        {
+            password_up_message.innerHTML = "✓ Upper characters";
+            password_up_message.style.color = "green";
+        }
+        if (numNums < 1)
+        {
+            password_num_message.innerHTML = "✗ Numbers";
+            password_num_message.style.color = "red";
+        } else 
+        {
+            password_num_message.innerHTML = "✓ Numbers";
+            password_num_message.style.color = "green";
+        }
+        if (numSpecials < 1)
+        {
+            password_spe_message.innerHTML = "✗ Special characters";
+            password_spe_message.style.color = "red";
+        } else 
+        {
+            password_spe_message.innerHTML = "✓ Special characters";
+            password_spe_message.style.color = "green";
+        }
+  }
+
+  CheckPasswordsMatch(event) {
+    const password_match_message = document.getElementById("password_match_message");
+    var password = (document.getElementById("newPassword") as HTMLInputElement).value;
+    var passwordConfirmation = (document.getElementById("newPasswordConfirmation") as HTMLInputElement).value;
+
+    if (password !== passwordConfirmation)
+        {
+          password_match_message.innerHTML = "✗ Passwords don't match";
+          password_match_message.style.color = "red";
+        } else 
+        {
+          password_match_message.innerHTML = "✓ Passwords match";
+          password_match_message.style.color = "green";
+        }
+  }
 
   constructor (public activatedRoute: ActivatedRoute, 
               public updateNameService: UpdateNameService,
