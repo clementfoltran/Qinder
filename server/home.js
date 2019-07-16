d3efbf9f3d3af79e2666cd465b56b809e77c6abc
clementfoltran <clfoltra@student.42.fr>
2019-07-16 12:58:27 +0200

    [REFACTOR] match logic

--- server/home.js
+++ server/home.js
@@ -106,14 +106,16 @@ exports.swipe = async (req, res) => {
       let sql = 'INSERT INTO swipe VALUES(id_swipe, ?, ?, ?, NULL)';
       let query = db.format(sql, [ req.body.id_user, req.body.id_user_, req.body.like ]);
       await db.query(query, (err) => {
+        console.log(req.body.like);
         if (err) {
           throw err
         } else if (req.body.like) {
           sql = 'SELECT id_user FROM swipe WHERE id_user = ? AND id_user_matched = ? AND swipe.like = 1';
-          query = db.format(sql, [ req.body.id_user, req.body.id_user_ ]);
+          query = db.format(sql, [ req.body.id_user_, req.body.id_user ]);
           db.query(query, (err, response) => {
             if (err) throw err;
             else if (response[0]) {
+              console.log(response[0].id_user);
               const insertMatch = 'INSERT INTO `match` VALUES(id_match, NOW())';
               query = db.format(insertMatch);
               db.query(query, (err, response) => {
@@ -128,6 +130,8 @@ exports.swipe = async (req, res) => {
                   });
                 }
               });
+            } else {
+              res.json({ success: true, message: '', match: false });
             }
           });
         } else {
